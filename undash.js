e6702f1d087d3ee32ee0bae66042659d65bc2c84
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2020-09-16 21:39:22 +0530

    five methods related to object types

--- undash.js
+++ undash.js
@@ -1,9 +1,33 @@
 var _=(function(){
 
     var methods={}
+
+    methods.objectType=function(obj){
+        return Object.prototype.toString.call(obj)
+    }
     
-    methods.wish=function(){
-        return "Welcome to undash!"
+    //Checks if the passed object is a function.
+    methods.isFunc=function(obj){
+        return (typeof(obj)==="function")? true:false;
+    }
+    
+    //Checks if the passed object is a Number.
+    methods.isNum=function(obj){
+        return (!isNaN(obj)?true:false)
+    }
+
+    //Checks if the passed object is a Null.
+    methods.isNull= function(obj){
+        return obj===null;
+    }
+
+    //Checks if the passed object is a Undefined.
+    methods.isUndefined=function(obj){
+        return obj===undefined
+    }
+    
+    methods.isBoolean=function(obj){
+        return obj===true || obj===false || toString.call(obj) === '[object Boolean]'
     }
     return methods;
     })()
